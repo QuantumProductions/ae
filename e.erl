82c06f6e7fd6439e1af8d645537793decf9f58ee
quantumpotato <quantumpotato@gmail.com>
2017-07-27 12:50:02 -0400

    Basic requirement test

--- e.erl
+++ e.erl
@@ -1,14 +1,22 @@
 -module(e).
 -export([a/1]).
+-include_lib("eunit/include/eunit.hrl").
 
 % player info passed in
 % Next, PlayerInfo ..
 add(B) when is_binary(B) -> B;
-add({r, V, O, C, List}) ->
-  case requirement:met(V, O, C, PlayerInfo) of
+add({r, N, List}) ->
+  case N =< 4 of
     true -> a(List);
     false -> no
   end.
+
+
+% add({r, V, O, C, List}) ->
+%   case requirement:met(V, O, C, PlayerInfo) of
+%     true -> a(List);
+%     false -> no
+%   end.
 
 % assemble
 a(List) ->
@@ -20,3 +28,9 @@ a([H | T], List) ->
     no -> a(T, List);
     V -> a(T, lists:append(List, [V]))
   end.
+
+basic_test() ->
+  R = e:a([{r, 1, [<<"cavern">>]},
+   <<"The bats blink battily">>,  
+   {r, 5, <<"You do not see the invisible wizard!">>}]),
+  [[<<"cavern">>],<<"The bats blink battily">>] = R.
